b3e20d6747c0571887efbfc030ebe4ddc58783ff
Kedar Agate <kedar.agate@gmail.com>
2022-10-16 14:24:35 +0530

    m

--- myApp.js
+++ myApp.js
@@ -2,6 +2,10 @@ const { application } = require("express");
 require("dotenv").config();
 let express = require("express");
 let app = express();
+let bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app
@@ -10,7 +14,7 @@ app
     res.json({ name: `${req.query.first} ${req.query.last}` });
   })
   .post((req, res) => {
-    console.log(req.query);
+    console.log(req.body);
   });
 
 module.exports = app;
