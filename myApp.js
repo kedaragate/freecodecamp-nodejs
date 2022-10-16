ea4dd13a785426343d190b14ecb261ecfe074964
Kedar Agate <kedar.agate@gmail.com>
2022-10-16 09:40:41 +0530

    m

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 const { application } = require("express");
 let express = require("express");
 let app = express();
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/views/index.html");
+// });
 
 module.exports = app;
