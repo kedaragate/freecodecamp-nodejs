5569ba2f0899769cab4160d1b46677e9b76a509f
Kedar Agate <kedar.agate@gmail.com>
2022-10-16 13:19:12 +0530

    m

--- myApp.js
+++ myApp.js
@@ -4,15 +4,9 @@ let express = require("express");
 let app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", (req, res) => {
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json({ message: "Hello Json".toUpperCase() });
-  } else {
-    res.json({ message: "Hello Json" });
-  }
-});
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+app.use("/json", (req, res, next) => {
+  console.log(`${req.method} /${req.path} - ${req.ip}`);
+  next();
 });
 
 module.exports = app;
