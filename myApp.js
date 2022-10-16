fb6861f5de1fe3ed0e1b306df8ab3e2187189ce3
Kedar Agate <kedar.agate@gmail.com>
2022-10-16 10:30:36 +0530

    .env

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 const { application } = require("express");
+require("dotenv").config();
 let express = require("express");
 let app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", (req, res) => {
-  res.json({ message: "Hello json" });
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  }
 });
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
