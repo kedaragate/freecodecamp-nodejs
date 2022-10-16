9e2e5eda5b91218bbb9c5c8cc4a4aba37d4c2487
Kedar Agate <kedar.agate@gmail.com>
2022-10-16 09:57:48 +0530

    m

--- myApp.js
+++ myApp.js
@@ -3,8 +3,8 @@ let express = require("express");
 let app = express();
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+app.get("/json", (req, res) => {
+  res.json({ message: "Hello json" });
 });
 
 module.exports = app;
